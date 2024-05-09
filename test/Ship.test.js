ad96d28cefccd0714b410a06111d1024ff795e34
bilelhamitouche <bilelhamitouche4@gmail.com>
2024-05-09 12:28:19 +0100

    fixed typo in the test

--- test/Ship.test.js
+++ test/Ship.test.js
@@ -1,9 +1,8 @@
-
 import Ship from '../src/classes/Ship';
 
 test('Testing if Ship class exists', () => {
   expect(Ship).toBeDefined();
-}); 
+});
 
 test('Testing if calling with new works as intended', () => {
   expect(new Ship()).toEqual({
@@ -20,7 +19,7 @@ test('Testing if calling with new works as intended', () => {
   expect(new Ship().isSunk).toBeDefined();
 });
 
-describe('Testing if the builtin methods works', () => {
+describe('Testing if the builtin methods work', () => {
   test('Testing if the hit() method works', () => {
     const ship1 = new Ship();
     expect(ship1.hit()).toBe(1);
